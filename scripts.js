75d763b76f06036606544d7d33f3b940e43558b0
KaKa <sk2504@naver.com>
2018-11-01 17:05:25 +0900

    lexer와 arrayParser함수의 기능을 구현 (#95)
    lexer 함수를 통해 입력받은 문자열형태의 배열을 한개씩 조건에 맞게 분류하여
    result 라는 배열에 넣어 반환, 그값을 arrayParser에서 분석해 객체형태로 반환하는 형식

--- scripts.js
+++ scripts.js
@@ -1,64 +1,26 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-
-const init = () => {
-  rl.question('분석할 JSON 데이터를 입력하세요. ', (answer) => {
-    let arr = answer.split(' ');
-    let res = [];
-    arrays(arr, res);
-    console.log(result(res));
-    rl.close();
-  });
-}
-
-
-const arrays = (arr, res) => {
-  if (arr[0] === '[' && arr[arr.length - 1] === ']') {
-    for (let i = 1; i < arr.length - 1; i++) {
-      arr[i].match(',') ? res.push(arr[i].replace(',', "")) : res.push(arr[i]);
-    }
+function lexer(str) {
+  let result = []
+  let number = ''
+  for (i = 0; i < str.length; i++) {
+    if (str[i].match(/\]/)) result.push(number);
+    if (str[i].match(/\[|\]/)) result.push(`${str[i]}`);
+    if (str[i].match(/[^\[\],]/)) number += str[i];
+    if (str[i].match(/,/)) {
+      result.push(number);
+      number = '';
+    };
+  };
+  return result;
+};
+function arrayParser(str) {
+  let lexerResult = lexer(str);
+  let parserResult = {};
+  if (lexerResult[0] + lexerResult[lexerResult.length - 1] === '[]') parserResult.type = 'array'
+  parserResult.child = [];
+  for (i = 1; i < lexerResult.length - 1; i++) {
+    if (Number(lexerResult[i])) parserResult.child[i - 1] = { type: 'number', value: `${lexerResult[i]}`, child: [] }
   }
-  return res;
-}
-
-
-const types = {
-  string: function (res) {
-    let strs = [];
-    for (let val of res) {
-      if (val.match('"')) {
-        strs.push(val.replace('"', "").replace('"', ""));
-      }
-    }
-    return strs.length;
-  },
-  numbers: function (res) {
-    let nums = [];
-    for (let val of res) {
-      if (!isNaN(Number(val))) {
-        nums.push(Number(val));
-      }
-    }
-    return nums.length;
-  },
-  bools: function (res) {
-    let bools = [];
-    for (let val of res) {
-      if (val === "false" || val === "true") {
-        bools.push(val);
-      }
-    }
-    return bools.length;
-  }
-}
-
-const result = (res) => {
-  return "총 " + res.length + "개의 데이터 중에 문자열 " + types.string(res) + "개, 숫자 " + types.numbers(res) + "개, 부울 " + types.bools(res) + "개가 포함되어 있습니다.";
-}
-
-
-init();
+  return parserResult;
+};
+let result = arrayParser('[11,332,132]');
+console.log(JSON.stringify(result, null, 2));

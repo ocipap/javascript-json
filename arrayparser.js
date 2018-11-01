3eb5ee906a730f968d1df3de4fae57cb239fc6ab
cohily12 <k96079123@gmail.com>
2018-11-01 17:46:22 +0900

    [Torco] Step6 - 오류 상황 탐지 (#97)
    * add Error class in tokenizer.js
    
    * add Error case for arrayClose
    
    * add Error case for object close
    
    * add Error case for colon missed
    
    * add processByType method in tokenizer class
    
    * add error case in object

--- arrayparser.js
+++ arrayparser.js
@@ -3,10 +3,10 @@ const Tokenizer = require("./tokenizer.js");
 const lexer = require("./lexer.js");
 
 class Data {
-  constructor(type, value, child = []) {
+  constructor(type, value, child) {
     this.type = type;
     this.value = value;
-    this.child = child;
+    if (child) this.child = child;
   }
 }
 
@@ -24,7 +24,7 @@ function arrayParser(str) {
     const value = lexeme.value;
 
     if (type === 'array' || type === 'object') {
-      stack.push(new Data(type, value));
+      stack.push(new Data(type, value, []));
     }
     else if (type === 'arrayClose' || type === 'objectClose') {
       parsedData = stack.pop();
@@ -33,7 +33,7 @@ function arrayParser(str) {
     }
     else {
       const top = stack.peek();
-      top.child.push(new Data(type, value, ''));
+      top.child.push(new Data(type, value));
       tempData = '';
     }
   }
@@ -43,6 +43,36 @@ function arrayParser(str) {
 /*
 Test Case
 */
-const str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]"
-const result = arrayParser(str);
-console.log(JSON.stringify(result, null, 2));
+// const str = "['1a3',[null,false,['11',112,'99', {a : 'str', b : [912,[5656,33]]}], true]]";;
+// const result = arrayParser(str);
+// console.log(JSON.stringify(result, null, 2));
+
+// const s1 = "['1a3',[null,false,['11',112,'99' , {a:'str', b:[912,[5656,33]]}, true]";
+// const result1 = arrayParser(s1);
+// console.log(JSON.stringify(result1, null, 2));
+// // 정상적으로 종료되지 않은 배열이 있습니다.
+
+// const s2 = "['1a3',[null,false,['11',112,'99'], {a:'str', b: [912,[5656,33]], true]";
+// const result2 = arrayParser(s2);
+// console.log(JSON.stringify(result2, null, 2));
+// // 정상적으로 종료되지 않은 객체가 있습니다.
+
+// const s3 = "['1a3',[null,false,['11',112,'99'], {a:'str', b  [912,[5656,33]]}, true]";
+// const result3 = arrayParser(s3);
+// console.log(JSON.stringify(result3, null, 2));
+// // ':'이 누락된 객체표현이 있습니다.
+
+// const s4 = "['1a3',[null,false,['11',112,'99'], {a:'str', b : }, true]";
+// const result4 = arrayParser(s4);
+// console.log(JSON.stringify(result4, null, 2));
+// // key의 value가 정의되지 않았습니다.
+
+// const s5 = "['1a3',[null,false,['11',112,'99'], , {a:'str', b  [912,[5656,33]]}, true]";
+// const result5 = arrayParser(s5);
+// console.log(JSON.stringify(result5, null, 2));
+// // 연속된 ','가 존재합니다.
+
+// const s6 = "['1a3',[null,false,['11',112,'99'], {a:'str', : [912,[5656,33]]}, true]";
+// const result6 = arrayParser(s6);
+// console.log(JSON.stringify(result6, null, 2));
+// // 연속된 ','가 존재합니다.
--- tokenizer.js
+++ tokenizer.js
@@ -4,53 +4,153 @@ module.exports = class Tokenizer {
         this.token = '';
         this.bStr = false;
         this.bStrOpen = false;
+        this.arrayStack = 0;
+        this.objectStack = 0;
+        this.key = '';
+        this.bKeyAvailable = false;
+        this.bValueAvailable = false;
+        this.type;
+        this.stateList = [];
+        this.error = new Error;
     }
+
     push(token) {
         this.tokenList.push(token);
     }
+
     initToken() {
         this.token = '';
     }
+
     concat(token, char) {
         this.token = token + char;
     }
-    throwWrongString() {
-        if (this.bStrOpen) throw `${this.token}은 올바른 문자열이 아닙니다.`;
+
+    processByType(char) {
+        this.type = {
+            '['() {
+                this.stateList.push('array');
+                this.arrayStack++;
+            },
+
+            '{'() {
+                this.stateList.push('object');
+                this.objectStack++;
+                this.bKeyAvailable = true;
+            },
+
+            ':'() {
+                if (!this.token.trim()) {
+                    this.error.throwNotDefinedKey();
+                }
+                else {
+                    this.key = this.token.trim();
+                    this.bKeyAvailable = false;
+                    this.bValueAvailable = true;
+                }
+            },
+
+            ','() {
+                if (!this.token.trim()) this.error.throwWrongComma();
+
+                const currentState = this.stateList[this.stateList.length - 1];
+                if (currentState === 'object' && this.bValueAvailable) {
+                    this.bKeyAvailable = true;
+                    this.bValueAvailable = false;
+                }
+            },
+
+            ']'() {
+                if (!this.arrayStack) this.error.throwWrongArray();
+
+                this.arrayStack--;
+                this.stateList.pop();
+            },
+
+            '}'() {
+                if (!this.objectStack) this.error.throwWrongObject();
+                if (this.bKeyAvailable && this.token.trim()) this.error.throwMissingColon();
+                if (this.bValueAvailable && !this.token.trim()) this.error.throwNotDefinedValue(this.key);
+
+                this.objectStack--;
+                this.stateList.pop();
+            }
+        }
+        this.type[char].call(this);
     }
+
     run(str) {
         for (let char of str) {
             if (this.bStrOpen) {
                 if (char === "'") {
-                    this.bStrOpen = !this.bStrOpen;
-                    this.bStr = !this.bStr;
+                    this.bStr = true;
+                    this.bStrOpen = false;
                 }
                 this.concat(this.token, char);
             }
             else if (char.match(/\[|\{|\:/)) {
+                this.processByType(char);
+
                 this.concat(this.token, char);
                 this.push(this.token.trim());
                 this.initToken();
             }
             else if (char.match(/,|\]|\}/)) {
-                this.throwWrongString();
+                if (this.bStr && this.bStrOpen) this.error.throwWrongString(this.token);
 
-                if (this.token) this.push(this.token.trim());
-                if (char !== ',') this.push(char);
+                this.processByType(char);
+
+                if (this.token.trim()) this.push(this.token.trim());
                 this.initToken();
+                if (char !== ',') this.token += char;
                 this.bStr = false;
             }
             else {
-                if (char === "'") this.bStrOpen = !this.bStrOpen;
+                if (char === "'") this.bStrOpen = true;
+                if (this.bStr && this.bStrOpen) this.error.throwWrongString(this.token);
+
                 this.concat(this.token, char);
-                if (this.bStr) this.throwWrongString();
             }
         }
 
-        //올바르지 않은 문자열 검출
-        this.throwWrongString();
+        //error 검출
+        if (this.bStrOpen) this.error.throwWrongString(this.token);
+        if (this.objectStack) this.error.throwWrongObject();
+        if (this.arrayStack) this.error.throwWrongArray();
 
-        if (this.token) this.push(this.token.trim());
+        if (this.token) this.push(this.token);
+
 
         return this.tokenList;
+    }
+}
+
+class Error {
+    throwWrongString(string) {
+        throw `${string}은 올바른 문자열이 아닙니다.`;
+    }
+
+    throwWrongArray() {
+        throw `정상적으로 종료되지 않은 배열이 있습니다.`;
+    }
+
+    throwWrongObject() {
+        throw `정상적으로 종료되지 않은 객체가 있습니다.`;
+    }
+
+    throwNotDefinedKey() {
+        throw `key가 정의되지 않았습니다.`
+    }
+
+    throwNotDefinedValue(key) {
+        throw `${key}의 value가 정의되지 않았습니다.`
+    }
+
+    throwMissingColon() {
+        throw `':'이 누락된 객체표현이 있습니다.`;
+    }
+
+    throwWrongComma() {
+        throw `연속된 ','가 존재합니다.`
     }
 }

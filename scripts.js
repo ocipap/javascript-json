c2b832d0c80a7289f5a70ccadefc9aba629e871d
KaKa <sk2504@naver.com>
2018-11-02 17:59:56 +0900

    2중 배열paser기능 구현 (#98)
    lexer에 조건을 추가하여],]로 끝나는 경우도 체크하게하고,
    parser에[조건을 추가하여 배열을 인식하게 하였고, lexerResult를 순회하는 변수와
    parserResult 를 순회하는 변수를 2개를 두어 반복문에서 사용할수있게 하였다.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 function lexer(str) {
-  let result = []
-  let number = ''
+  let result = [];
+  let number = '';
   for (i = 0; i < str.length; i++) {
-    if (str[i].match(/\]/)) result.push(number);
+    if (str[i] === ']' && str[i - 1] !== ']') result.push(number);
     if (str[i].match(/\[|\]/)) result.push(`${str[i]}`);
     if (str[i].match(/[^\[\],]/)) number += str[i];
-    if (str[i].match(/,/)) {
-      result.push(number);
-      number = '';
+    if (str[i] === ',') {
+      if (str[i - 1] === ']') {
+        number = '';
+      } else {
+        result.push(number);
+        number = '';
+      }
     };
   };
   return result;
@@ -15,12 +19,24 @@ function lexer(str) {
 function arrayParser(str) {
   let lexerResult = lexer(str);
   let parserResult = {};
-  if (lexerResult[0] + lexerResult[lexerResult.length - 1] === '[]') parserResult.type = 'array'
+  if (lexerResult[0] + lexerResult[lexerResult.length - 1] === '[]') parserResult.type = 'array';
   parserResult.child = [];
-  for (i = 1; i < lexerResult.length - 1; i++) {
-    if (Number(lexerResult[i])) parserResult.child[i - 1] = { type: 'number', value: `${lexerResult[i]}`, child: [] }
-  }
+  for (let i = 1, j = 1; i < lexerResult.length - 1; i++ , j++) {
+    if (Number(lexerResult[i])) {
+      parserResult.child[j - 1] = { type: 'number', value: `${lexerResult[i]}`, child: [] };
+    }
+    if (lexerResult[i] !== '[') continue;
+    parserResult.child[j - 1] = { type: 'array', value: 'ArrayObject', child: [] };
+    for (let k = i + 1; k < lexerResult.length; k++) {
+      if (lexerResult[k] === ']') {
+        i = k;
+        break;
+      };
+      parserResult.child[j - 1].child.push({ type: 'number', value: lexerResult[k], child: [] });
+    };
+  };
   return parserResult;
 };
-let result = arrayParser('[11,332,132]');
+let result = arrayParser('[11,332,[1],[1,2,3],132,[1,2,3],[3],[4]]');
+
 console.log(JSON.stringify(result, null, 2));

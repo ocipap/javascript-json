f8779c82f567d43d8ad236f3745d657a0d73ea4b
KaKa <sk2504@naver.com>
2018-11-13 18:25:05 +0900

    Step4 PR (#106)
    * reducer 위치변경 및 checkNum if문부분 변경
    
    reducer 함수를 arrayParser 위로 변경하고 checkNum의 부분을 함수로뺀걸
    한줄로 변경
    
    * null,boll,string 타입을 지원한다
    
    mapper 함수안에 if문을 추가하여 다양한 타입을 지원하게끔 하였다
    
    * 리팩토링 및 checktype함수추가
    
    mapper안에 코드를 checktype함수로 분리하였다
    
    * 문법오류 검사기능 추가
    
    ex) "1a3 같은 문자열토큰이있을떄 갯수를 파악해 짝수가 아닐경우
    오류를 반환하게 했다.'
    '
    
    * bug 수정및 getbool 함수추가
    
    1a3 같은 문자열토큰을 판단할때 str인지, 선언되지않은값인지 판단하는
    과정에 버그를 수정하였고, null,false,true 값이val 로 들어오면
    NaN체크를 하지않고바로 리턴되게 만들었다.
    
    * 피드백 사항 수정 및 deepCopyObh함수추가
    
    deepCopyobj 함수를 추가해, eavl대신 작동하고, checkToken 함수안에 if문에 사용된
    정규표현식을 줄였다.그외 함수명 수정및 리팩토링

--- scripts.js
+++ scripts.js
@@ -4,67 +4,24 @@ class Stack {
     this.endCount = -1;
   }
 };
-function arrayParser(str) {
-  let tokenArray = lexer(tokenize, str);
-  let result = tokenArray.reduce(reducer, tokenArray[0]);
-  return result;
-};
-function lexer(fn, str) {
-  let tokenArray = fn(str);
-  let lexerResult = tokenArray.map(v => {
-    if (v === '[') return { type: 'array', value: 'ArrayObject', child: [] };
-    if (Number(v)) return { type: 'number', value: `${v}`, child: [] };
-    return v;
-  });
-  return lexerResult;
-};
-function tokenize(str) {
-  let result = each(str, checkToken);
-  return result;
-};
-function each(str, iter) {
-  let result = [];
-  let number;
-  for (i = 0; i < str.length; i++) {
-    number = iter(str, result, number);
-  }
-  return result;
-};
-function checkToken(str, result, number) {
-  if (number === undefined) number = '';
-  if (str[i] === ']' && str[i - 1] !== ']') result.push(number);
-  if (str[i].match(/\[|\]/)) result.push(`${str[i]}`);
-  if (str[i].match(/[^\[\],]/)) number += str[i];
-  if (str[i] === ',') {
-    if (str[i - 1] === ']') {
-      number = '';
-    } else {
-      result.push(number);
-      number = '';
-    }
-  };
-  return number;
-};
-function reducer(acc, cur) {
+function tokenReducer(acc, cur) {
   arg = [acc, cur];
-  return checkNum(...arg) || checkArray(...arg) || checkEnd(...arg) || acc;
+  return checkNumOrStr(...arg) || checkArray(...arg) || checkEnd(...arg) || acc;
 };
-function checkNum(acc, cur) {
-  if (cur.type === 'number') return pushChild(...arguments);
-  return false;
-};
-function pushChild(acc, cur) {
-  acc.child.push(cur);
-  return acc;
-};
-function checkArray(acc, cur) {
-  if (cur.type === 'array') {
-    stack.list.push(acc);
-    stack.endCount++;
-    acc = cur;
+function checkNumOrStr(acc, cur) {
+  if (cur.type !== 'array' && cur !== ']') {
+    acc.child.push(cur);
     return acc;
   }
   return false;
+};
+function checkArray(acc, cur) {
+  if (cur.type !== 'array') return false;
+  stack.list.push(acc);
+  stack.endCount++;
+  acc = cur;
+  return acc;
+
 };
 function checkEnd(acc, cur) {
   if (cur === ']' && stack.endCount !== 0) {
@@ -75,8 +32,94 @@ function checkEnd(acc, cur) {
   }
   return false;
 };
+function arrayParser(str) {
+  let tokenArray = lexer(tokenize, str);
+  console.log('입력하신 str에 대한 분석결과는...')
+  if (tokenArray) return tokenArray.reduce(tokenReducer, tokenArray[0]);
+  return [];
+};
+function lexer(fn, str) {
+  let tokenArray = fn(str);
+  if (checkSyntax(tokenArray)) return false;
+  let lexerResult = tokenArray.map(tokenMapper);
+  return lexerResult;
+};
+function checkSyntax(tokenArray) {
+  var result = tokenArray.some(v => {
+    if (checkQuote(v)) return true;
+    if (checkNaN(v)) return true;
+  })
+  return result ? true : false;
+}
+function checkQuote(val) {
+  if (val.match(/'|"/g) !== null && val.match(/'|"/g).length % 2 !== 0) {
+    console.error(`${val} 는 올바른 값이 아닙니다`)
+    return true;
+  }
+  return false;
+}
+function checkNaN(val) {
+  if (getBoolean(val)) return false;
+  if (val.match(/'|"/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
+    console.log(`${val}는 알수없는 타입임니다.`)
+    return true;
+  }
+  if (typeof val === 'string' && val !== ']') return false;
+}
+function getBoolean(val) {
+  val = val.match(/\S\w*/g)
+  return type['bool'].some(function (bool) {
+    return bool === val[0];
+  });
+}
+const type = {
+  '[': { type: 'array', value: 'ArrayObject', child: [] },
+  'null': { type: 'Null', value: `null`, child: [] },
+  'true': { type: 'Boolean', value: `true`, child: [] },
+  'false': { type: 'Boolean', value: `false`, child: [] },
+  'bool': ['true', 'false', 'null']
+};
+function tokenMapper(value) {
+  let conversionValue = value.match(/[^\s]\w*'|[^\s]\w*/)[0];
+  if (checktype(conversionValue)) return deepCopyObj(checktype(conversionValue));
+  if (Number(conversionValue)) return { type: 'number', value: `${conversionValue}`, child: [] };
+  if (typeof conversionValue === 'string' && conversionValue !== ']') {
+    return { type: 'string', value: `${conversionValue}`, child: [] };
+  }
+  return conversionValue;
+};
+function deepCopyObj(obj) {
+  let copiedObj = JSON.parse(JSON.stringify(obj))
+  return copiedObj;
+};
+function checktype(value) {
+  if (type[value]) return type[value];
+  return false;
+};
+function tokenize(str) {
+  let result = each(str, checkToken);
+  return result;
+};
+function each(str, iter) {
+  let result = [];
+  let token;
+  for (let i = 0; i < str.length; i++) {
+    token = iter(str, result, token, i);
+  }
+  return result;
+};
+function checkToken(str, result, token, i) {
+  if (token === undefined) token = '';
+  if (str[i] === ']' && str[i - 1] !== ']') result.push(token);
+  if (str[i] === ']' || str[i] === '[') result.push(`${str[i]}`);
+  if (str[i].match(/[^\[\],]/)) token += str[i];
+  if (str[i] === ',' && str[i - 1] === ']') token = '';
+  if (str[i] === ',' && str[i - 1] !== ']') { result.push(token); token = ''; }
+  return token;
+};
 
-let str = "[1,[1,[1,[1,3,[1,[2,[11233,[4,[5,[6,[7],1],3],4]]]],6]]], 22]";
+let str = "['3a3',[null, false, ['11', [['null',[false]]], 'a112'],55,'99'],33,true]";
+// let str = '[1,2,[3,[4,[5],1],2],3]';
 let stack = new Stack;
 let result = arrayParser(str);
 console.log(JSON.stringify(result, null, 2));

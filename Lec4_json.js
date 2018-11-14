dd290344982bb1ae2135819e50898f5239fa40eb
Soom <40848630+developersoom@users.noreply.github.com>
2018-11-14 14:58:45 +0900

    step2 (#109)

--- Lec4_json.js
+++ Lec4_json.js
@@ -1,63 +1,78 @@
-let tokens = {
-    types: [],
-    values: []
-}
-
-let result = {
-    type: ""
-}
-
-class Child {
-    constructor(type, value, child) {
-        this.type = type;
-        this.value = value;
-        this.child = child
-    }
-}
-
 function scan(str) {
+    let tokens = [];
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === '[' || str[i] === ']') {
-            tokens.types.push(str[i])
-            continue;
-        }
-        if (str[i] === ' ') {
-            continue;
-        }
-        tokens.values.push(str[i])
+        tokens.push(str[i])
     }
     return tokens;
 }
 
-function parse(str) {
-    if (tokens.types.length === 2) {
-        result.type = 'array';
-        result.child = [];
+function checkType(tokens) {
+    if (tokens[0] === '[' && tokens[tokens.length - 1] === ']') {
+        return 'array';
+    }
+    if (Number(tokens)) {
+        return 'number';
+    }
+}
+
+function divideTokens(str) {
+    const tokens = scan(str);
+    let values = [];
+    let emptyArr = [];
+    for (let i = 0; i < tokens.length; i++) {
+        if (i === tokens.length - 1) {
+            emptyArr.push(tokens[i]);
+            values.push(emptyArr.join(''));
+            break;
+        }
+        if (tokens[i] !== ',') {
+            emptyArr.push(tokens[i]);
+            continue;
+        }
+        values.push(emptyArr.join(''));
+        emptyArr = [];
     }
 
-    let leftValues = [...tokens.values]
-    let type = "";
-    let value = "";
+    return values;
+}
 
-    for (let i = 0; i < tokens.values.length; i++) {
-        if (Number(tokens.values[i])) {
-            value += tokens.values[i];
-            type = "number";
-            leftValues.shift();
-            if(leftValues.length){
-            continue;}
+function removeBrackets(str) {
+    let stringToArr = str.split('');
+    stringToArr.pop();
+    stringToArr.shift();
+    return stringToArr.join('');
+}
+
+
+function parse(str) {
+    let result = {}
+    result.type = checkType(str);
+    if (result.type === 'array') {
+        str = removeBrackets(str);
+        result.child = divideTokens(str);
+    }
+    if (result.type === 'number') {
+        result.value = divideTokens(str).join('');
+        result.child = [];
+    }
+    return result;
+}
+
+function getResult(str) {
+    let result = parse(str);
+    if (result.child !== []) {
+        for (let i = 0; i < result.child.length; i++) {
+            result.child[i] = parse(result.child[i]);
         }
-        if (tokens.values[i] === ',' || !leftValues.length) {
-            let child1 = new Child(type, value, []);
-            result.child.push(child1);
-            leftValues.shift();
-            type = "";
-            value = "";
-            continue;
+    }
+    for (let i = 0; i < result.child.length; i++) {
+        if (result.child[i].type === 'array') {
+            result.child[i].child = parse(result.child[i].child)
         }
     }
     return result;
 }
 
-let str = '[123,22,33]'
-console.log(parse(scan(str)))
+//test
+const str = '[123,[22],33]'
+console.log(getResult(str))

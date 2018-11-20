700ea2c0c0651f90f2c363509b97e6aaaa4b3939
Soom <40848630+developersoom@users.noreply.github.com>
2018-11-20 17:08:13 +0900

    [숨] lec4 - step4 (#117)
    * Edit parse() - add conditions to check boolean, string, null
    
    * Add countApostrophe() to check errors
    
    * Edit parse() - add countApostrophe() to check string errors
    
    * Edit parse() - add an error statement for string w/o ''
    
    * Edit parse() - give values(type, value, child) as arguments
    
    * Remove useless lines
    
    * Edit scan() - delete toString()
    
    * Remove 'continue'
    
    * Edit parse() - change if condition for checking numbers
    
    * Edit scan() - remove if condition for checking blanks

--- Lec4_json.js
+++ Lec4_json.js
@@ -13,14 +13,12 @@ function scan(str) {
         if (token === ',' || token === '[') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
             stack = "";
-            continue;
-        }
-        if (token === ']') {
+        } else if (token === ']') {
             tokens.push(stack);
             stack = token;
-            continue;
+        } else {
+            stack += token;
         }
-        stack += token;
     }
     return tokens;
 }
@@ -28,26 +26,57 @@ function scan(str) {
 function parse(str) {
     const tokens = scan(str);
     let result = [];
-    let type = "", value = "", child = [];
+    let type = "",
+        value = "",
+        child = [];
 
     for (let token of tokens) {
         if (token === '[') {
-            type = 'array';
-            result.push(new Data(type, value, child));
-        } else if (Number(token)) {
+            result.push(new Data('array', value, child));
+        } else if ((!isNaN(Number(token)))) {
             const lastChild = result[result.length - 1].child;
-            type = 'number', value = token;
-            lastChild.push(new Data(type, value));
+            lastChild.push(new Data('number', token));
+            type = "", value = "", child = [];
+        } else if (token === 'null') {
+            const lastChild = result[result.length - 1].child;
+            lastChild.push(new Data('null', token));
+            type = "", value = "", child = [];
+        } else if (token === 'true' || token === 'false') {
+            const lastChild = result[result.length - 1].child;
+            lastChild.push(new Data('boolean', token));
+            type = "", value = "", child = [];
+        } else if (token[0] === "'" && token[token.length - 1] === "'") {
+            if (!countApostrophe(token)) {
+                console.log(`${token}은 올바른 문자열이 아닙니다.`);
+                return;
+            }
+            const lastChild = result[result.length - 1].child;
+            lastChild.push(new Data('string', token));
             type = "", value = "", child = [];
         } else if (token === ']' && result.length > 1) {
             const lastData = result.pop();
             const lastChild = result[result.length - 1].child;
             lastChild.push(lastData);
+        } else {
+            console.log(`${token}은 올바른 문자열이 아닙니다.`);
+            return;
         }
+
     }
     return result;
 }
 
+function countApostrophe(token) {
+    let count = 0;
+    for (let letter of token) {
+        if (letter === "'") count++;
+    }
+    if (count === 2) return true;
+    return false;
+}
+
 //test
-var str = "[123,[22,23,[11,[112233],112],55],33]";
-console.log(JSON.stringify(parse(str), null, 2));
+// var str = "[123,[22,23,[11,[112233],112],55],33]";
+var str = "['d1',[22,23,[11,[112233],112],55],'3d3']"
+console.log(parse(str))
+// console.log(JSON.stringify(parse(str), null, 2));

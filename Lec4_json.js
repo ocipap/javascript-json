94bf4ab9a1a0aacaced5dd253020aed879f69e8b
Soom <40848630+developersoom@users.noreply.github.com>
2018-11-29 10:41:57 +0900

    [숨] lec4 - step5 (#123)
    * Edit scan() - add conditions checking curly brackets
    
    * Edit scan() - add condition checking blanks
    
    * Edit scan() - add condition for checking array inside object
    
    * Edit parse() - add conditions checking objects / yet get error for nested array
    
    * Edit scan() - edit condition checking ':'
    
    * Edit scan() - add condition to include last square bracket
    
    * Edit parse() - add condition checking object inside array && Edit scan() - delete condition checking last square bracket
    
    * Edit scan() - refactoring
    
    * Edit scan() - divide condition checking comma to avoid it being tokens
    
    * Edit parse() - works for object with one key value, but get error for more than two keys
    
    * Edit parse() - works for objects with more than two keys
    
    * Edit parse() - delete useless variables
    
    * Fix errors
    
    * Add tokenMap
    
    * Add tokenChecker
    
    * Move varialbe 'objectStatus' into global
    
    * Add parseToken
    
    * Edit parse() - apply changes
    
    * Fix typo
    
    * Fix errors

--- Lec4_json.js
+++ Lec4_json.js
@@ -10,12 +10,18 @@ function scan(str) {
     let tokens = [];
     let stack = "";
     for (let token of str) {
-        if (token === ',' || token === '[') {
+        if (token === '[' || token === '{') {
             stack === "" ? tokens.push(token) : tokens.push(stack);
             stack = "";
-        } else if (token === ']') {
-            tokens.push(stack);
-            stack = token;
+        } else if(token === ','){
+            if(stack !== "") {tokens.push(stack)};
+            stack = "";
+        } else if (token === ']' || token === '}' || token === ':') {
+            if(stack !== ""){tokens.push(stack)};
+            tokens.push(token);
+            stack = "";
+        } else if (token === ' ') {
+            continue;
         } else {
             stack += token;
         }
@@ -23,47 +29,68 @@ function scan(str) {
     return tokens;
 }
 
+const tokenMap = {
+    others : {":" : "objectKey", "null" : "null", "true" : "boolean", "false" : "boolean", "'" : "string"},
+    start : {"[" : "array", "{" : "object"},
+    end : {"]" : "array", "}" : "object"}
+}
+
+const tokenChecker = {
+    isStartToken(token){
+        if(Object.keys(tokenMap.start).includes(token)) return tokenMap.start[token];
+    }, 
+    isEndToken(token, result){
+        if(Object.keys(tokenMap.end).includes(token) && result.length > 1) return tokenMap.end[token];
+    },
+    isOtherToken(token){
+        if(Object.keys(tokenMap.others).includes(token)) return tokenMap.others[token];
+        if(token[0] === "'") return tokenMap.others[token[0]];
+        if(!isNaN(Number(token))) return "number";
+    },
+    isFinalToken(token, result){
+        if(token === ']' && result.length === 1) return true;
+    }
+}
+
+let objectStatus = false;
+
+const parseToken = {
+    executeStartToken(result, tokenType){
+        result.push(new Data(tokenType, "", []));
+        if(tokenType === 'object') objectStatus = true;
+    },
+    executeOtherToken(result, tokenType, token, objectKeyName){
+        const lastChild = result[result.length - 1].child;
+        if(tokenType === 'objectKey') lastChild.push(new Data(tokenType, objectKeyName));
+        else lastChild.push(new Data(tokenType, token));
+    },
+    executeEndToken(result, tokenType){
+        const lastData = result.pop();
+        const lastChild = result[result.length - 1].child;
+        lastChild.push(lastData);
+        if(tokenType === 'object') objectStatus = false;
+    }
+}
+
 function parse(str) {
     const tokens = scan(str);
     let result = [];
-    let type = "",
-        value = "",
-        child = [];
+    let objectKeyName;
+    let tokenType;
 
-    for (let token of tokens) {
-        if (token === '[') {
-            result.push(new Data('array', value, child));
-        } else if ((!isNaN(Number(token)))) {
-            const lastChild = result[result.length - 1].child;
-            lastChild.push(new Data('number', token));
-            type = "", value = "", child = [];
-        } else if (token === 'null') {
-            const lastChild = result[result.length - 1].child;
-            lastChild.push(new Data('null', token));
-            type = "", value = "", child = [];
-        } else if (token === 'true' || token === 'false') {
-            const lastChild = result[result.length - 1].child;
-            lastChild.push(new Data('boolean', token));
-            type = "", value = "", child = [];
-        } else if (token[0] === "'" && token[token.length - 1] === "'") {
-            if (!countApostrophe(token)) {
-                console.log(`${token}은 올바른 문자열이 아닙니다.`);
-                return;
-            }
-            const lastChild = result[result.length - 1].child;
-            lastChild.push(new Data('string', token));
-            type = "", value = "", child = [];
-        } else if (token === ']' && result.length > 1) {
-            const lastData = result.pop();
-            const lastChild = result[result.length - 1].child;
-            lastChild.push(lastData);
-        } else {
-            console.log(`${token}은 올바른 문자열이 아닙니다.`);
-            return;
-        }
+    for (let token of tokens){
+        if (tokenChecker.isFinalToken(token, result)) return result;
+        
+        if (tokenType = tokenChecker.isStartToken(token)) parseToken.executeStartToken(result, tokenType);
+        
+        else if (tokenType = tokenChecker.isOtherToken(token)) parseToken.executeOtherToken(result, tokenType, token, objectKeyName);
+        
+        else if (tokenType = tokenChecker.isEndToken(token, result)) parseToken.executeEndToken(result, tokenType);
 
-    }
-    return result;
+        else if (objectStatus && token !== ':') objectKeyName = token;
+        
+        else {console.log(`${token}은 올바른 문자열이 아닙니다.`); return;}
+    } 
 }
 
 function countApostrophe(token) {
@@ -76,7 +103,9 @@ function countApostrophe(token) {
 }
 
 //test
-// var str = "[123,[22,23,[11,[112233],112],55],33]";
-var str = "['d1',[22,23,[11,[112233],112],55],'3d3']"
-console.log(parse(str))
-// console.log(JSON.stringify(parse(str), null, 2));
+
+var str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a: 'a' }, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]";
+// var str = "[1,{a:'str', b:[912,[5656,33]]}]";
+// var str = "[1,{key: [2,{a:'a'}]}]"
+// console.log(parse(str))
+console.log(JSON.stringify(parse(str), null, 2));

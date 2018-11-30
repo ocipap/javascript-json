2a519537bd6e7cced1ff6da59e23fa95fefc69b7
KaKa <sk2504@naver.com>
2018-11-30 09:04:06 +0900

    Step5 (#112)
    * 객체타입을 분류할수있다
    
    checktoken에 객체를 확인하는 조건을 추가하고, strack에객체안의 객체도 판단할수있게,
    objcount 추가, 객체가 끝날대까지 token을더하고 count가 0이되면 token을 리턴,
    완성된 객체토큰을 mapper에서 객체로 판단하고, 그때
    Json.parse를 이용해 객체로 반환한다.
    
    *  object type 추가
    
    checktype 함수에 객체가 들어오면 object타입을가진  child에 토큰을 넣어 반환하게 변경
    
    * 리팩토링 및 checkObjToken함수추가
    
    checktoken 함수의 if문을 checkObjtoken 함수로 분리,리팩토링
    
    * createArr 함수,createLiteral함수,arrReducer함수 추가
    
    객체타입이 들어왔을때 객체 리터럴 형태로 반환하도록 일단 구현함.
    
    * createLiteral 함수이름을 inObjTokenMapper 로변경 및 리펙토링
    
    객체안의 배열은 arrayParser 함수를 다시 호출하여 해결
    
    * 리펙토링및 일부if문 변경

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ class Stack {
   constructor() {
     this.list = [];
     this.endCount = -1;
+    this.objCount = 0;
   }
 };
 function tokenReducer(acc, cur) {
@@ -21,7 +22,6 @@ function checkArray(acc, cur) {
   stack.endCount++;
   acc = cur;
   return acc;
-
 };
 function checkEnd(acc, cur) {
   if (cur === ']' && stack.endCount !== 0) {
@@ -34,10 +34,15 @@ function checkEnd(acc, cur) {
 };
 function arrayParser(str) {
   let tokenArray = lexer(tokenize, str);
-  console.log('입력하신 str에 대한 분석결과는...')
-  if (tokenArray) return tokenArray.reduce(tokenReducer, tokenArray[0]);
+  if (tokenArray) {
+    stackInit();
+    return tokenArray.reduce(tokenReducer, tokenArray[0]);
+  }
   return [];
 };
+function stackInit() {
+  stack = new Stack;
+}
 function lexer(fn, str) {
   let tokenArray = fn(str);
   if (checkSyntax(tokenArray)) return false;
@@ -50,38 +55,37 @@ function checkSyntax(tokenArray) {
     if (checkNaN(v)) return true;
   })
   return result ? true : false;
-}
+};
 function checkQuote(val) {
   if (val.match(/'|"/g) !== null && val.match(/'|"/g).length % 2 !== 0) {
-    console.error(`${val} 는 올바른 값이 아닙니다`)
+    console.error(`${val} 는 올바른 값이 아닙니다`);
     return true;
   }
   return false;
-}
+};
 function checkNaN(val) {
   if (getBoolean(val)) return false;
-  if (val.match(/'|"/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
-    console.log(`${val}는 알수없는 타입임니다.`)
+  if (val.match(/'|"|\{/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
+    console.log(`${val}는 알수없는 타입임니다.`);
     return true;
   }
   if (typeof val === 'string' && val !== ']') return false;
-}
+};
 function getBoolean(val) {
   val = val.match(/\S\w*/g)
-  return type['bool'].some(function (bool) {
-    return bool === val[0];
-  });
-}
+  return type['bool'].some(function (bool) { return bool === val[0]; });
+};
 const type = {
   '[': { type: 'array', value: 'ArrayObject', child: [] },
   'null': { type: 'Null', value: `null`, child: [] },
   'true': { type: 'Boolean', value: `true`, child: [] },
   'false': { type: 'Boolean', value: `false`, child: [] },
-  'bool': ['true', 'false', 'null']
+  'bool': ['true', 'false', 'null'],
 };
 function tokenMapper(value) {
-  let conversionValue = value.match(/[^\s]\w*'|[^\s]\w*/)[0];
-  if (checktype(conversionValue)) return deepCopyObj(checktype(conversionValue));
+  if (value.match(/{/g)) return checkType(value, 'obj');
+  let conversionValue = value.match(/[^\s]\w*('|")|[^\s]\w*/)[0]
+  if (checkType(conversionValue)) return deepCopyObj(checkType(conversionValue));
   if (Number(conversionValue)) return { type: 'number', value: `${conversionValue}`, child: [] };
   if (typeof conversionValue === 'string' && conversionValue !== ']') {
     return { type: 'string', value: `${conversionValue}`, child: [] };
@@ -89,13 +93,38 @@ function tokenMapper(value) {
   return conversionValue;
 };
 function deepCopyObj(obj) {
-  let copiedObj = JSON.parse(JSON.stringify(obj))
+  let copiedObj = JSON.parse(JSON.stringify(obj));
   return copiedObj;
 };
-function checktype(value) {
+function checkType(value, obj = 0) {
+  if (obj) return returnObj(value);
   if (type[value]) return type[value];
   return false;
 };
+function returnObj(value) {
+  let result = { type: 'object', value: null };
+  result.value = createObj(value);
+  return result;
+};
+function createObj(value) {
+  let [result, key, val, keyCount, arrayCount] = [{}, '', '', 0, 0];
+  for (indx in value) {
+    let token = value[indx]
+    if (token === '{' && arrayCount === 0) continue;
+    if (token === ' ') continue;
+    if (token === '[') arrayCount++;
+    if (token === '}' && arrayCount === 0 || token === ',' && arrayCount === 0) {
+      result[key] = inObjTokenMapper(val);
+      [key, val, keyCount] = ['', '', 0];
+    }
+    if (arrayCount !== 0) val += token;
+    if (keyCount === 0 && token !== ':' && token !== ',') key += token;
+    if (keyCount !== 0 && token !== '}' && arrayCount === 0) val += token;
+    if (token === ']' && arrayCount !== 0) arrayCount--;
+    if (token === ':') keyCount++;
+  }
+  return result;
+}
 function tokenize(str) {
   let result = each(str, checkToken);
   return result;
@@ -108,18 +137,35 @@ function each(str, iter) {
   }
   return result;
 };
+function checkObjToken(str, token, i) {
+  if (str[i] === '{' || stack.objCount !== 0) {
+    if (str[i] === '{') stack.objCount++;
+    token += str[i];
+    if (str[i] === '}') stack.objCount--;
+    return token;
+  }
+  return false;
+};
 function checkToken(str, result, token, i) {
   if (token === undefined) token = '';
+  if (checkObjToken(str, token, i)) return checkObjToken(str, token, i);
   if (str[i] === ']' && str[i - 1] !== ']') result.push(token);
   if (str[i] === ']' || str[i] === '[') result.push(`${str[i]}`);
   if (str[i].match(/[^\[\],]/)) token += str[i];
   if (str[i] === ',' && str[i - 1] === ']') token = '';
-  if (str[i] === ',' && str[i - 1] !== ']') { result.push(token); token = ''; }
+  if (str[i] === ',' && str[i - 1] !== ']' && stack.objCount === 0) { result.push(token); token = ''; }
   return token;
 };
-
-let str = "['3a3',[null, false, ['11', [['null',[false]]], 'a112'],55,'99'],33,true]";
-// let str = '[1,2,[3,[4,[5],1],2],3]';
+function inObjTokenMapper(val) {
+  if (isNaN(val) && val.match(/\{|\[/g) === null && !['null', 'false', 'ture'].includes(val)) {
+    return { type: 'string', value: `${val}` }
+  }
+  if (!isNaN(val)) return { type: 'number', value: `${val}` };
+  if (val === 'true' || val === 'false' || val === 'null') return type[val];
+  if (val.match(/^\[/g)) return arrayParser(val);
+  if (val.match(/\{/g)) return createObj(val);
+};
+let str = '[ {a : [1,{b : 123}], c : "str"},[1,[2,[{d : 123}],4],5], 6]';
 let stack = new Stack;
 let result = arrayParser(str);
 console.log(JSON.stringify(result, null, 2));

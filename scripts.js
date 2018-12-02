ae9a6f597360345c79159b74bdd1fe9553fa2853
KaKa <sk2504@naver.com>
2018-12-03 08:10:46 +0900

    Step6 (#126)
    * initstack 함수명 수정및 리팩토링
    
    initStack함수를 initializeStack로 이름변경 및 inObjTokenMapper 함수내의 if문 안의 조건을
    checkString, checkBool 함수로 변경
    
    * _Array, type class를 생성한다.
    
    class화를 위해 _array와 Type class를 만들어, 기존 전역변수이던 type값을 Type class에 편입,
    _Array class는 기존 arrayparser 함수와 부수함수들을 모두 프로토타입에 넣어 생성, 프로퍼티로는
    stack , type class를 가지게 하여 arrayparser 실행시 값을 이용할수있게 변경.
    기존 오류처리에 , 객체 속의 value값에도 적용될수있게 코드수정도 함
    
    * checkBracket함수구현
    
    checkBracket함수를 통해 arr 의 대괄호의 갯수를파악, 잘못됫을시 오류를 반환하게 구현
    
    * checkKeytype 함수추가
    
    key타입에 허용되지않는 값이 들어오면 오류반환

--- scripts.js
+++ scripts.js
@@ -5,167 +5,198 @@ class Stack {
     this.objCount = 0;
   }
 };
-function tokenReducer(acc, cur) {
-  arg = [acc, cur];
-  return checkNumOrStr(...arg) || checkArray(...arg) || checkEnd(...arg) || acc;
-};
-function checkNumOrStr(acc, cur) {
-  if (cur.type !== 'array' && cur !== ']') {
-    acc.child.push(cur);
-    return acc;
-  }
-  return false;
-};
-function checkArray(acc, cur) {
-  if (cur.type !== 'array') return false;
-  stack.list.push(acc);
-  stack.endCount++;
-  acc = cur;
-  return acc;
-};
-function checkEnd(acc, cur) {
-  if (cur === ']' && stack.endCount !== 0) {
-    stack.list[stack.endCount].child.push(acc);
-    acc = stack.list[stack.endCount];
-    stack.endCount--;
-    return acc;
-  }
-  return false;
-};
-function arrayParser(str) {
-  let tokenArray = lexer(tokenize, str);
-  if (tokenArray) {
-    stackInit();
-    return tokenArray.reduce(tokenReducer, tokenArray[0]);
-  }
-  return [];
-};
-function stackInit() {
-  stack = new Stack;
-}
-function lexer(fn, str) {
-  let tokenArray = fn(str);
-  if (checkSyntax(tokenArray)) return false;
-  let lexerResult = tokenArray.map(tokenMapper);
-  return lexerResult;
-};
-function checkSyntax(tokenArray) {
-  var result = tokenArray.some(v => {
-    if (checkQuote(v)) return true;
-    if (checkNaN(v)) return true;
-  })
-  return result ? true : false;
-};
-function checkQuote(val) {
-  if (val.match(/'|"/g) !== null && val.match(/'|"/g).length % 2 !== 0) {
-    console.error(`${val} 는 올바른 값이 아닙니다`);
-    return true;
-  }
-  return false;
-};
-function checkNaN(val) {
-  if (getBoolean(val)) return false;
-  if (val.match(/'|"|\{/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
-    console.log(`${val}는 알수없는 타입임니다.`);
-    return true;
-  }
-  if (typeof val === 'string' && val !== ']') return false;
-};
-function getBoolean(val) {
-  val = val.match(/\S\w*/g)
-  return type['bool'].some(function (bool) { return bool === val[0]; });
-};
-const type = {
-  '[': { type: 'array', value: 'ArrayObject', child: [] },
-  'null': { type: 'Null', value: `null`, child: [] },
-  'true': { type: 'Boolean', value: `true`, child: [] },
-  'false': { type: 'Boolean', value: `false`, child: [] },
-  'bool': ['true', 'false', 'null'],
-};
-function tokenMapper(value) {
-  if (value.match(/{/g)) return checkType(value, 'obj');
-  let conversionValue = value.match(/[^\s]\w*('|")|[^\s]\w*/)[0]
-  if (checkType(conversionValue)) return deepCopyObj(checkType(conversionValue));
-  if (Number(conversionValue)) return { type: 'number', value: `${conversionValue}`, child: [] };
-  if (typeof conversionValue === 'string' && conversionValue !== ']') {
-    return { type: 'string', value: `${conversionValue}`, child: [] };
-  }
-  return conversionValue;
-};
-function deepCopyObj(obj) {
-  let copiedObj = JSON.parse(JSON.stringify(obj));
-  return copiedObj;
-};
-function checkType(value, obj = 0) {
-  if (obj) return returnObj(value);
-  if (type[value]) return type[value];
-  return false;
-};
-function returnObj(value) {
-  let result = { type: 'object', value: null };
-  result.value = createObj(value);
-  return result;
-};
-function createObj(value) {
-  let [result, key, val, keyCount, arrayCount] = [{}, '', '', 0, 0];
-  for (indx in value) {
-    let token = value[indx]
-    if (token === '{' && arrayCount === 0) continue;
-    if (token === ' ') continue;
-    if (token === '[') arrayCount++;
-    if (token === '}' && arrayCount === 0 || token === ',' && arrayCount === 0) {
-      result[key] = inObjTokenMapper(val);
-      [key, val, keyCount] = ['', '', 0];
+class Type {
+  constructor() {
+    this.result = {
+      '[': { type: 'array', value: 'ArrayObject', child: [] },
+      'null': { type: 'Null', value: `null`, child: [] },
+      'true': { type: 'Boolean', value: `true`, child: [] },
+      'false': { type: 'Boolean', value: `false`, child: [] },
+      'bool': ['true', 'false', 'null'],
     }
-    if (arrayCount !== 0) val += token;
-    if (keyCount === 0 && token !== ':' && token !== ',') key += token;
-    if (keyCount !== 0 && token !== '}' && arrayCount === 0) val += token;
-    if (token === ']' && arrayCount !== 0) arrayCount--;
-    if (token === ':') keyCount++;
   }
-  return result;
-}
-function tokenize(str) {
-  let result = each(str, checkToken);
-  return result;
 };
-function each(str, iter) {
-  let result = [];
-  let token;
-  for (let i = 0; i < str.length; i++) {
-    token = iter(str, result, token, i);
+class _Array {
+  constructor() {
+    this.stack = new Stack;
+    this.type = new Type;
+  };
+  tokenReducer(acc, cur) {
+    let arg = [acc, cur];
+    return this.checkNumOrStr(...arg) || this.checkArray(...arg) || this.checkEnd(...arg) || acc;
+  };
+  checkNumOrStr(acc, cur) {
+    if (cur.type !== 'array' && cur !== ']') {
+      acc.child.push(cur);
+      return acc;
+    }
+    return false;
+  };
+  checkArray(acc, cur) {
+    if (cur.type !== 'array') return false;
+    this.stack.list.push(acc);
+    this.stack.endCount++;
+    acc = cur;
+    return acc;
+  };
+  checkEnd(acc, cur) {
+    if (cur === ']' && this.stack.endCount !== 0) {
+      this.stack.list[this.stack.endCount].child.push(acc);
+      acc = this.stack.list[this.stack.endCount];
+      this.stack.endCount--;
+      return acc;
+    }
+    return false;
+  };
+  parser(str) {
+    this.checkBracket(str);
+    let tokenArray = this.lexer(this.tokenize.bind(this), str);
+    if (tokenArray) {
+      this.initializeStack();
+      return tokenArray.reduce(this.tokenReducer.bind(this), tokenArray[0]);
+    }
+    return [];
+  };
+  checkBracket(str) {
+    let wrongArrBracket = str.match(/\[|\]/g).length % 2;
+    if (wrongArrBracket) throw new Error('제대로 종료되지 않은 배열이 있습니다!(대괄호의 갯수가 맞지 않아요)');
+    let wrongObjBracket = str.match(/\{|\}/g).length % 2;
+    if (wrongObjBracket) throw new Error('제대로 종료되지 않은 객체가 있습니다!(중괄호의 갯수가 맞지 않아요)');
   }
-  return result;
-};
-function checkObjToken(str, token, i) {
-  if (str[i] === '{' || stack.objCount !== 0) {
-    if (str[i] === '{') stack.objCount++;
-    token += str[i];
-    if (str[i] === '}') stack.objCount--;
+  initializeStack() {
+    this.stack = new Stack;
+  };
+  lexer(fn, str) {
+    let tokenArray = fn(str);
+    if (this.checkSyntax(tokenArray)) return false;
+    let lexerResult = tokenArray.map(this.tokenMapper.bind(this));
+    return lexerResult;
+  };
+  checkSyntax(tokenArray) {
+    var result = tokenArray.some(v => {
+      if (this.checkQuote(v)) return true;
+      if (this.checkNaN(v)) return true;
+    })
+    return result ? true : false;
+  };
+  checkQuote(val) {
+    if (val.match(/'|"/g) !== null && val.match(/'|"/g).length % 2 !== 0) {
+      throw new Error(`${val} 는 올바른 값이 아닙니다`);
+    }
+    return false;
+  };
+  checkNaN(val) {
+    if (this.getBoolean(val)) return false;
+    if (val.match(/'|"|\{/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
+      throw new Error(`${val}는 알수없는 타입 입니다.`);
+    }
+    if (typeof val === 'string' && val !== ']') return false;
+  };
+  getBoolean(val) {
+    val = val.match(/\S\w*/g);
+    return this.type.result['bool'].some(bool => bool === val[0]);
+  };
+  tokenMapper(value) {
+    if (value.match(/{/g)) return this.checkType(value, 'obj');
+    let conversionValue = value.match(/[^\s]\w*('|")|[^\s]\w*/)[0];
+    if (this.checkType(conversionValue)) return this.deepCopyObj(this.checkType(conversionValue));
+    if (Number(conversionValue)) return { type: 'number', value: `${conversionValue}`, child: [] };
+    if (typeof conversionValue === 'string' && conversionValue !== ']') {
+      return { type: 'string', value: `${conversionValue}`, child: [] };
+    }
+    return conversionValue;
+  };
+  deepCopyObj(obj) {
+    let copiedObj = JSON.parse(JSON.stringify(obj));
+    return copiedObj;
+  };
+  checkType(value, obj = 0) {
+    if (obj) return this.returnObj(value);
+    if (this.type.result[value]) return this.type.result[value];
+    return false;
+  };
+  returnObj(value) {
+    let result = { type: 'object', value: null };
+    result.value = this.createObj(value);
+    return result;
+  };
+  createObj(value) {
+    let [result, key, val, keyCount, arrayCount] = [{}, '', '', 0, 0];
+    for (let indx in value) {
+      let token = value[indx];
+      if (token === '{' && arrayCount === 0) continue;
+      if (token === ' ') continue;
+      if (token === '[') arrayCount++;
+      if (token === '}' && arrayCount === 0 || token === ',' && arrayCount === 0) {
+        this.checkKeyType(key);
+        result[key] = this.inObjTokenMapper(val);
+        [key, val, keyCount] = ['', '', 0];
+      }
+      if (arrayCount !== 0) val += token;
+      if (keyCount === 0 && token !== ':' && token !== ',') key += token;
+      if (keyCount !== 0 && token !== '}' && arrayCount === 0) val += token;
+      if (token === ']' && arrayCount !== 0) arrayCount--;
+      if (token === ':') keyCount++;
+    }
+    return result;
+  }
+  checkKeyType(key){
+    if(key.match(/\[|\{|\]|\}/g)) throw new Error('key 값에 허용되지 않는 값이 있습니다.');
+  };
+  tokenize(str) {
+    let result = this.each(str, this.checkToken.bind(this));
+    return result;
+  };
+  each(str, iter) {
+    let result = [];
+    let token;
+    for (let i = 0; i < str.length; i++) {
+      token = iter(str, result, token, i);
+    }
+    return result;
+  };
+  checkObjToken(str, token, i) {
+    if (str[i] === '{' || this.stack.objCount !== 0) {
+      if (str[i] === '{') this.stack.objCount++;
+      token += str[i];
+      if (str[i] === '}') this.stack.objCount--;
+      return token;
+    }
+    return false;
+  };
+  checkToken(str, result, token, i) {
+    if (token === undefined) token = '';
+    if (this.checkObjToken(str, token, i)) return this.checkObjToken(str, token, i);
+    if (str[i] === ']' && str[i - 1] !== ']') result.push(token);
+    if (str[i] === ']' || str[i] === '[') result.push(`${str[i]}`);
+    if (str[i].match(/[^\[\],]/)) token += str[i];
+    if (str[i] === ',' && str[i - 1] === ']') token = '';
+    if (str[i] === ',' && str[i - 1] !== ']' && this.stack.objCount === 0) { result.push(token); token = ''; }
     return token;
-  }
-  return false;
+  };
+  inObjTokenMapper(val) {
+    this.checkInObjValue(val);
+    if (this.checkString(val)) return { type: 'string', value: `${val}` };
+    if (!isNaN(val)) return { type: 'number', value: `${val}` };
+    if (this.checkBool(val)) return this.type.result[val];
+    if (val.match(/^\[/g)) return this.parser(val);
+    if (val.match(/\{/g)) return this.createObj(val);
+  };
+  checkInObjValue(val) {
+    this.checkQuote(val);
+    this.checkNaN(val);
+  };
+  checkString(val) {
+    const bool = ['null', 'false', 'ture'];
+    if (isNaN(val) && val.match(/\{|\[/g) === null && !bool.includes(val)) return true;
+    return false;
+  };
+  checkBool(val) {
+    const bool = ['true', 'flase', 'null'];
+    return bool.includes(val);
+  };
 };
-function checkToken(str, result, token, i) {
-  if (token === undefined) token = '';
-  if (checkObjToken(str, token, i)) return checkObjToken(str, token, i);
-  if (str[i] === ']' && str[i - 1] !== ']') result.push(token);
-  if (str[i] === ']' || str[i] === '[') result.push(`${str[i]}`);
-  if (str[i].match(/[^\[\],]/)) token += str[i];
-  if (str[i] === ',' && str[i - 1] === ']') token = '';
-  if (str[i] === ',' && str[i - 1] !== ']' && stack.objCount === 0) { result.push(token); token = ''; }
-  return token;
-};
-function inObjTokenMapper(val) {
-  if (isNaN(val) && val.match(/\{|\[/g) === null && !['null', 'false', 'ture'].includes(val)) {
-    return { type: 'string', value: `${val}` }
-  }
-  if (!isNaN(val)) return { type: 'number', value: `${val}` };
-  if (val === 'true' || val === 'false' || val === 'null') return type[val];
-  if (val.match(/^\[/g)) return arrayParser(val);
-  if (val.match(/\{/g)) return createObj(val);
-};
-let str = '[ {a : [1,{b : 123}], c : "str"},[1,[2,[{d : 123}],4],5], 6]';
-let stack = new Stack;
-let result = arrayParser(str);
+let str = '[ "a" , {a : ["1a3", {b : "2as3"}], c : "str"}, [1, [2, [{d : null}], 4], 5], 6]';
+let _array = new _Array();
+let result = _array.parser(str);
 console.log(JSON.stringify(result, null, 2));

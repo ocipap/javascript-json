79faa286728caa87d4a7ccfdf1b4fdaa70569fc1
Soom <40848630+developersoom@users.noreply.github.com>
2018-12-03 10:35:01 +0900

    [숨] lec4 - step6 (#127)
    * Add errorChecker - checking if array or object is closed
    
    * Edit parse() - edit conditions for checking errors
    
    * Edit parse() - change to for-loop to use index
    
    * Add condition checking colon in object
    
    * Add condition to count colon
    
    * Add errorCheck - if object key is with apostrophes
    
    * Remove useless
    
    * Fix errors

--- Lec4_json.js
+++ Lec4_json.js
@@ -32,17 +32,19 @@ function scan(str) {
 const tokenMap = {
     others : {":" : "objectKey", "null" : "null", "true" : "boolean", "false" : "boolean", "'" : "string"},
     start : {"[" : "array", "{" : "object"},
-    end : {"]" : "array", "}" : "object"}
+    end : {"]" : "array", "}" : "object"},
+    count : {"[" : 0, "{" : 0, "]" : 0, "}" : 0, ":" : 0}
 }
 
 const tokenChecker = {
     isStartToken(token){
-        if(Object.keys(tokenMap.start).includes(token)) return tokenMap.start[token];
+        if(Object.keys(tokenMap.start).includes(token)) tokenMap.count[token]++; return tokenMap.start[token];
     }, 
     isEndToken(token, result){
-        if(Object.keys(tokenMap.end).includes(token) && result.length > 1) return tokenMap.end[token];
+        if(Object.keys(tokenMap.end).includes(token) && result.length > 1) tokenMap.count[token]++; return tokenMap.end[token];
     },
     isOtherToken(token){
+        if(token === ":") tokenMap.count[':']++;
         if(Object.keys(tokenMap.others).includes(token)) return tokenMap.others[token];
         if(token[0] === "'") return tokenMap.others[token[0]];
         if(!isNaN(Number(token))) return "number";
@@ -72,24 +74,44 @@ const parseToken = {
     }
 }
 
+const errorChecker = {
+    isArrayClosed(){
+        return tokenMap.count["["] === tokenMap.count["]"];
+    },
+    isObjectClosed(){
+        return tokenMap.count["{"] === tokenMap.count["}"];
+    },
+    isColonInObject(){
+        return tokenMap.count['{'] <= tokenMap.count[":"];
+    }
+}
 function parse(str) {
     const tokens = scan(str);
-    let result = [];
-    let objectKeyName;
-    let tokenType;
+    let result = [], objectKeyName, tokenType;
 
-    for (let token of tokens){
-        if (tokenChecker.isFinalToken(token, result)) return result;
+    for (let i = 0 ; i < tokens.length; i++){
+        let token = tokens[i];
         
+        if (i === tokens.length-1) {
+            tokenMap.count[token]++;
+            if (tokenChecker.isFinalToken(token, result) && errorChecker.isArrayClosed() && errorChecker.isObjectClosed() && errorChecker.isColonInObject()) return result;
+            else if (!errorChecker.isObjectClosed()) {console.log(`정상적으로 종료되지 않은 객체가 있습니다.`); return;}
+            else if (!errorChecker.isArrayClosed()) {console.log(`정상적으로 종료되지 않은 배열이 있습니다.`); return;}
+            else if (!errorChecker.isColonInObject()) {console.log(`':'이 누락된 객체표현이 있습니다.`); return;}
+        }
+            
         if (tokenType = tokenChecker.isStartToken(token)) parseToken.executeStartToken(result, tokenType);
         
-        else if (tokenType = tokenChecker.isOtherToken(token)) parseToken.executeOtherToken(result, tokenType, token, objectKeyName);
-        
+        else if (tokenType = tokenChecker.isOtherToken(token)) {
+            if(tokenType === 'objectKey' && objectKeyName === undefined){console.log(`올바른 object key 값이 아닙니다.`); return;} 
+            parseToken.executeOtherToken(result, tokenType, token, objectKeyName);
+        } 
+
         else if (tokenType = tokenChecker.isEndToken(token, result)) parseToken.executeEndToken(result, tokenType);
 
         else if (objectStatus && token !== ':') objectKeyName = token;
         
-        else {console.log(`${token}은 올바른 문자열이 아닙니다.`); return;}
+        else if (!countApostrophe(token)) {console.log(`${token}은 올바른 문자열이 아닙니다.`); return;}
     } 
 }
 
@@ -103,9 +125,7 @@ function countApostrophe(token) {
 }
 
 //test
-
-var str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a: 'a' }, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]";
-// var str = "[1,{a:'str', b:[912,[5656,33]]}]";
-// var str = "[1,{key: [2,{a:'a'}]}]"
+var str = "['1a3',[null,false,['11',112,'99'], {a:'str', b: [912,[5656,33]], true]";
+// var str = "[{a : 'b'}]";
 // console.log(parse(str))
 console.log(JSON.stringify(parse(str), null, 2));

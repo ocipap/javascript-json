4c5dd6f93dadf52a3321b83327d95039af756029
PAPICO <ocipap0531@gmail.com>
2019-05-03 21:10:31 +0900

    feat : laxer 함수 구현
    laxer 함수를 이용해 넘겨받은 문자열의 type을 정한다.
    
    - separator 배열에서 객체로 변경
    - validator 함수를 이용해 해당 함수에 type 프로퍼티 추가
    - checker 함수를 이용해 valudator 함수 리스트 순차 실행

--- arrayParser.js
+++ arrayParser.js
@@ -1,34 +1,69 @@
 const util = require('./util')
+const it = util.it
 
-const separator = ["[", ",", "]"]
+const separator = {
+    "arrayOpen" : "[",
+    "comma" : ",",
+    "arrayClose" : "]"
+}
 
-const isSeparator = (s) => util.inArray(s, separator)
+const isSeparator = (s) => util.inArray(s, Object.values(separator))
+
+const literalValidator = () => {
+    let validators = []
+    validators.push(util.validator("number", it.isNumber))
+    return validators
+}
+
+const typedLiteral = (s) => {
+    const literalChecker = util.checker.apply(null, literalValidator())
+    const type = literalChecker(s)
+    if(!type) throw "지원하지 않는 타입"
+    const typed = {
+        type,
+        value: s,
+        child: [],
+    }
+    return typed
+}
+
+const typedSeperator = (s) => {
+    const [type, value] = util.findOne(([k, v]) => v === s, Object.entries(separator))
+    const typed = {
+        type,
+        value,
+        child: []
+    }
+    return typed
+}
 
 const tokenizer = (text) => {
     let data = ""
     const tokenizedArr = []
     for (const s of text) {
-        if (isSeparator(s)) {
-            if(s === ",") {
-                tokenizedArr.push(data)
-            }
-            else {
-                (data !== "") ? tokenizedArr.push(data, s) : tokenizedArr.push(s)
-            }
+        if (s === ",") {
+            tokenizedArr.push(data)
+            data = ""
+        } else if (isSeparator(s)) {
+            (data !== "") ? tokenizedArr.push(data, s): tokenizedArr.push(s)
             data = ""
         } else {
-            if(s.trim() !== "" || data !== "") data += s
+            if (s.trim() !== "" || data !== "") data += s
         }
     }
     return tokenizedArr
 }
 
-const lexer = () => {
-    
+const lexer = (tokenizedArr) => {
+    return util.go(
+        tokenizedArr,
+        util.map(s => s.trim()),
+        util.map(s => isSeparator(s) ? typedSeperator(s) : typedLiteral(s))
+    )
 }
 
-const parser = () => {
-
+const parser = (laxeredArr) => {
+    
 }
 
 const arrayParser = (jsonString) => {
@@ -36,8 +71,9 @@ const arrayParser = (jsonString) => {
         util.go(
             jsonString,
             tokenizer,
+            lexer,
         )
     )
 }
 
-arrayParser("[123, 23, ssddd, aas]")
+arrayParser("[123, 13, 10]")

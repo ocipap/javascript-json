0c296174829faafc25c30e9ad2bbd3988d7393f9
papico <ocipap0531@gmail.com>
2019-05-15 20:34:52 +0900

    feat : tokenizer 구현
    - tokenizerHelper 함수 구현

--- 6-3/arrayParser.js
+++ 6-3/arrayParser.js
@@ -2,9 +2,38 @@ const _ = require('./util')
 const L = _.L
 const c = require('./checker')
 
-const tokenizer = () => {}
+const isSeparator = _.checker(
+    c.isRBracket,
+    c.isLBracket,
+    c.isLBrace,
+    c.isRBrace,
+    c.isColon
+)
 
-const laxer = () => {}
+const tokenizerHelper = (arr, s) => {
+    if (isSeparator(s)) {
+        arr.push(s)
+    } else if (c.isComma(s)) {
+        arr.push("")
+    } else {
+        let last = _.last(arr)
+        isSeparator(last) ? arr.push(s) : arr[arr.length - 1] = last.concat(s)
+    }
+    return arr
+}
+
+const tokenizer = (str) => _.reduce(tokenizerHelper, [], str)
+
+const laxer = (tokenedArr) => {}
 
 const parser = () => {}
 
+const ArrayParser = _.pipe(
+    tokenizer,
+    //laxer,
+    //parser
+)
+
+const str = "[123, 22, 33]";
+const result = ArrayParser(str);
+//console.log(JSON.stringify(result, null, 2));

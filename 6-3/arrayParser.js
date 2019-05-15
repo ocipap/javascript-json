40a469ac84443d46d20be1f9e8b2feaf50fe26f1
papico <ocipap0531@gmail.com>
2019-05-16 02:17:15 +0900

    feat : laxer 구현
    - type 을 판별하는 laxer 함수 구현
    - Either 모나드를 이용한 에러 분기
    - separator 와 offerdType 배열로 변경

--- 6-3/arrayParser.js
+++ 6-3/arrayParser.js
@@ -1,14 +1,31 @@
 const _ = require('./util')
 const L = _.L
 const c = require('./checker')
+const Either = require('./Either')
 
-const isSeparator = _.checker(
+const separator = [
     c.isRBracket,
     c.isLBracket,
     c.isLBrace,
     c.isRBrace,
     c.isColon
-)
+]
+
+const offerdType = [
+    c.isRBracket,
+    c.isLBracket,
+    c.isLBrace,
+    c.isRBrace,
+    c.isColon,
+    c.isNull,
+    c.isBoolean,
+    c.isNumber,
+    c.isString
+]
+
+const isSeparator = _.checker(...separator)
+
+const isOfferedType = _.checker(...offerdType)
 
 const tokenizerHelper = (arr, s) => {
     if (isSeparator(s)) {
@@ -22,18 +39,48 @@ const tokenizerHelper = (arr, s) => {
     return arr
 }
 
-const tokenizer = (str) => _.reduce(tokenizerHelper, [], str)
+const checkType = text =>
+    isOfferedType(text) ? Either.right(text) : Either.left(`${text}는(은) 알 수 없는 타입입니다.`)
 
-const laxer = (tokenedArr) => {}
+const checkStringType = text =>
+    (c.isString(text) && _.counts(text, "'") > 2) ? Either.left(`${text}는(은) 올바른 문자열이 아닙니다.`) : Either.right(text)
+
+
+const getType = text => {
+    const [typeFunc] = _.findOne(f => f(text), offerdType)
+    return Either.right({
+        type: typeFunc.type,
+        value: text,
+        child: []
+    })
+}
+
+const tokenizer = (str) => _.go(
+    _.reduce(tokenizerHelper, [], str),
+    _.map(v => v.trim()),
+    _.reject(v => v === "")
+)
+
+const laxer = _.map(v => {
+    return Either.of(v)
+        .chain(checkType)
+        .chain(checkStringType)
+        .chain(getType)
+        .getOrElseThrow()
+})
 
 const parser = () => {}
 
 const ArrayParser = _.pipe(
     tokenizer,
-    //laxer,
+    laxer,
     //parser
 )
 
-const str = "[123, 22, 33]";
-const result = ArrayParser(str);
-//console.log(JSON.stringify(result, null, 2));
+
+try {
+    const str = "[123, {'hello': 'world'} 22, 33]";
+    const result = ArrayParser(str);
+} catch(e) {
+    console.log(e.message)
+}

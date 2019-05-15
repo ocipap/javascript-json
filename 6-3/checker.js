e128090c4af170e5dfb636c9301ed701256ae464
papico <ocipap0531@gmail.com>
2019-05-15 17:23:22 +0900

    feat : checker 추가
    - 정규식을 이용한 판별 함수
    - null, boolean, number 변환 함수

---
+++ 6-3/checker.js
@@ -0,0 +1,31 @@
+const isLBracket = c => /\[/.test(c)
+const isRBracket = c => /\]/.test(c)
+const isLBrace = c => /[{]/.test(c)
+const isRBrace = c => /[}]/.test(c)
+const isComma = c => /,/.test(c)
+const isColon = c => /:/.test(c)
+const isNull = c => /null/.test(c)
+const isBoolean = c => /true|false/.test(c)
+const isNumber = c => /^[0-9]*$/.test(c)
+const isString = c => /['][a-z0-9]*[']|["][a-z0-9]*["]/.test(c)
+const toBoolean = text => text === "true" ? true : false
+const toNumber = text => Number(text)
+const toNull = _ => null
+
+const c = {
+    isLBracket,
+    isRBracket,
+    isLBrace,
+    isRBrace,
+    isComma,
+    isColon,
+    isNull,
+    isBoolean,
+    isNumber,
+    isString,
+    toBoolean,
+    toNumber,
+    toNull
+}
+
+module.exports = c

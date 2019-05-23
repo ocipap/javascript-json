d3f411efcb82dee3666692da6ef81120958cf940
papico <ocipap0531@gmail.com>
2019-05-23 21:23:35 +0900

    feat : parser 함수 개발

--- 6-3/arrayParser.js
+++ 6-3/arrayParser.js
@@ -8,7 +8,7 @@ const separator = [
     c.isLBracket,
     c.isLBrace,
     c.isRBrace,
-    c.isColon
+    c.isColon,
 ]
 
 const offerdType = [
@@ -20,12 +20,19 @@ const offerdType = [
     c.isNull,
     c.isBoolean,
     c.isNumber,
-    c.isString
+    c.isString,
+    c.isComma
 ]
+
+
 
 const isSeparator = _.checker(...separator)
 
 const isOfferedType = _.checker(...offerdType)
+
+const isValidString = text => c.isString(text) && _.counts(text, "'") > 2
+
+const isValidParsedResult = res => !res || res.length
 
 const tokenizerHelper = (arr, s) => {
     if (isSeparator(s)) {
@@ -43,19 +50,90 @@ const checkType = text =>
     isOfferedType(text) ? Either.right(text) : Either.left(`${text}는(은) 알 수 없는 타입입니다.`)
 
 const checkStringType = text =>
-    (c.isString(text) && _.counts(text, "'") > 2) ? Either.left(`${text}는(은) 올바른 문자열이 아닙니다.`) : Either.right(text)
+    !isValidString(text) ? Either.right(text) : Either.left(`${text}는(은) 올바른 문자열이 아닙니다.`)
 
 
 const getType = text => {
     const [typeFunc] = _.findOne(f => f(text), offerdType)
+    if (typeFunc.change) text = typeFunc.change(text)
     return Either.right({
         type: typeFunc.type,
         value: text,
-        child: []
     })
 }
 
-const tokenizer = (str) => _.go(
+const parserHelper = (arr) => {
+    let iter = arr[Symbol.iterator]()
+    let firstCur = iter.next().value
+
+    return Either.right((function recur(parent, type) {
+        let cur = iter.next().value
+
+        if (cur.type === "RBracket" || cur.type === "RBrace") return parent
+
+        else if (cur.type === "array" || cur.type === "object") {
+            parent.child.push(recur({
+                type: cur.type,
+                child: []
+            }, cur.type))
+            return recur(parent, type)
+        } else {
+            if (type === "array") {
+                parent.child.push(cur)
+            } else {
+                let key = cur.value
+                let colon = iter.next().value
+                let value = iter.next().value
+                let type = value.type
+
+                if (type === "array" || type === "object") {
+                    value = recur({
+                        type,
+                        child: []
+                    }, type)
+                } else {
+                    value = value.value
+                }
+
+                parent.child.push({
+                    key,
+                    type,
+                    value
+                })
+            }
+
+            return recur(parent, type);
+        }
+
+    })({
+        type: firstCur.type,
+        child: []
+    }, firstCur.type))
+}
+
+const checkHelper = sep => (arr, el) => (!arr) ? false : (el === sep) ? (arr.push(el), arr) : (arr.length) ? (arr.pop(), arr) : false
+
+const checkBraket = (arr) => {
+    let result = _.reduce(checkHelper("["), [],
+        _.go(
+            arr,
+            _.filter(v => v.type === "array" || v.type === "RBracket"),
+            _.pluck('value'),
+        ))
+    return isValidParsedResult(result) ? Either.left(`정상적으로 종료되지 않은 배열이 있습니다.`) : Either.right(arr)
+}
+
+const checkBrace = (arr) => {
+    let result = _.reduce(checkHelper("{"), [],
+        _.go(
+            arr,
+            _.filter(v => v.type === "object" || v.type === "RBrace"),
+            _.pluck('value'),
+        ))
+    return isValidParsedResult(result) ? Either.left(`정상적으로 종료되지 않은 객체가 있습니다.`) : Either.right(arr)
+}
+
+const tokenizer = str => _.go(
     _.reduce(tokenizerHelper, [], str),
     _.map(v => v.trim()),
     _.reject(v => v === "")
@@ -69,18 +147,26 @@ const laxer = _.map(v => {
         .getOrElseThrow()
 })
 
-const parser = () => {}
+const parser = arr => {
+    return Either.of(arr)
+        .chain(checkBraket)
+        .chain(checkBrace)
+        .chain(parserHelper)
+        .getOrElseThrow()
+}
 
 const ArrayParser = _.pipe(
     tokenizer,
     laxer,
-    //parser
+    parser,
 )
 
-
 try {
-    const str = "[123, {'hello': 'world'} 22, 33]";
+    const str1 = "[123, 1234, [1, 2]]"
+    const str = "['1a3',[null,false,['11',[112233],{'easy' : ['hello', 'world']},112],55, '99'],{'a':'str', 'b':[912,[5656,33],{'key' : 'innervalue', 'newkeys': [1,2,3,4,5]}]}, true]";
+    const test = "[]"
     const result = ArrayParser(str);
-} catch(e) {
-    console.log(e.message)
+    console.log(JSON.stringify(result, null, 2));
+} catch (e) {
+    console.log(e)
 }
